cf408ed476cd9bd69d6ec37e187618ec8b55312e
Remigestin <gestin.remi@gmail.com>
2019-10-29 10:26:22 +0100

    team models

---
+++ models/Team.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TeamSchema = new Schema({
+    avatarUrl: String,
+    admins: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    description: String,
+    name: {type: String, required: true},
+    // boards: [{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Board'
+    // }],
+    members: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
+}, {timestamps: true});
+
+const Team = mongoose.model('Team', TeamSchema, 'Teams');
+module.exports = Team;
