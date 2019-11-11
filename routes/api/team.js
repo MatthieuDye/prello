170c43dd315cceb05e976f3fadbab1fa9d8875d2
Gestin <gestin.remi@gmail.com>
2019-11-11 16:36:26 +0100

    add some team routes

--- routes/api/team.js
+++ routes/api/team.js
@@ -7,6 +7,7 @@ const validateCreateTeamInput = require("../../validation/createTeam");
 
 // Load Team model
 const Team = require("../../models/Team");
+const User = require("../../models/User");
 
 // @route POST api/team/creation
 // @desc Create the team
@@ -40,15 +41,22 @@ router.post("/creation", (req, res) => {
     });
 });
 
+// @route PUT api/team/addmember/:teamId/:memberId
+// @desc add a user to the team
+// @access Auth users
 router.put("/addmember/:teamId/:memberId", (req, res) => {
 
     //TODO : if user in not a admin of the team : 403
 
     const team = Team.findById(req.params.teamId);
+    const user = User.findById(req.params.memberId);
+
     if (!team) {
         return res.status(404).json({teamName: "This team does not exists"});
     }
-
+    if (!user) {
+        return res.status(404).json({teamName: "This user does not exists"});
+    }
 
     Team
         .updateOne({_id: req.params.teamId}, {
@@ -60,6 +68,51 @@ router.put("/addmember/:teamId/:memberId", (req, res) => {
             }
         })
         .then(team => res.status(201).send({message: 'User successfully added to the team'}))
+        .catch(err => console.log(err));
+
+});
+
+router.delete("/removemember/:teamId/:memberId", (req, res) => {
+
+    //TODO : if user in not a admin of the team : 403
+
+    const team = Team.findById(req.params.teamId);
+    const user = User.findById(req.params.memberId);
+
+    if (!team) {
+        return res.status(404).json({teamName: "This team does not exists"});
+    }
+    if (!user) {
+        return res.status(404).json({teamName: "This user does not exists"});
+    }
+
+    Team
+        .updateOne({_id: req.params.teamId}, {
+            $pull: {
+                members: {
+                    idUser: req.params.memberId,
+                    isAdmin: req.body.isAdmin
+                }
+            }
+        })
+        .then(team => res.status(201).send({message: 'User successfully deleted of the team'}))
+        .catch(err => console.log(err));
+
+});
+
+router.delete("delete/:teamId", (req, res) => {
+
+    //TODO : if user in not a admin of the team : 403
+
+    const team = Team.findById(req.params.teamId);
+
+    if (!team) {
+        return res.status(404).json({teamName: "This team does not exists"});
+    }
+
+    Team
+        .deleteOne({_id: req.params.teamId})
+        .then(team => res.status(201).send({message: 'Team successfully deleted'}))
         .catch(err => console.log(err));
 
 });
