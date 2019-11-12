1b38dbc39b0922d54e10ade73e8e764837edd020
Nathan Guillaud <nathan.guillaud@orange.fr>
2019-11-12 15:12:03 +0100

    Change token duration

--- routes/api/users.js
+++ routes/api/users.js
@@ -102,7 +102,7 @@ router.post("/login", (req, res) => {
           payload,
           keys.secretOrKey,
           {
-            expiresIn: 31556926 // 1 year in seconds
+            expiresIn: 3600 // 1 hour in seconds
           },
           (err, token) => {
             res.json({
