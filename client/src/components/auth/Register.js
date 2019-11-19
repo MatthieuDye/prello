fe4d1d26e2609aab44128600b342fdaf99686b75
Alia Chawaf <alia.chawaf@etu.umontpellier.fr>
2019-11-19 18:33:29 +0100

    FIX - front - Register Page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -6,28 +6,8 @@ import {registerUser} from "../../actions/authActions";
 import classnames from "classnames";
 import {Formik} from "formik";
 import * as Yup from "yup";
-import {Button, Image, Container, Divider, Form, Grid, Header, Segment, Message} from "semantic-ui-react";
-
-const RegisterSchema = Yup.object().shape({
-    firstName: Yup.string()
-        .required("First name is required")
-        .min(2, 'First name must be between 2 and 50 characters')
-        .max(50, 'First name must be between 2 and 50 characters'),
-    lastName: Yup.string()
-        .required("Last name is required")
-        .min(2, 'Last name must be between 2 and 50 characters')
-        .max(50, 'Last name must be between 2 and 50 characters'),
-    userName: Yup.string()
-        .required("Username is required")
-        .min(3, 'Username must be between 3 and 30 characters')
-        .max(30, 'Username must be between 3 and 30 characters'),
-    email: Yup.string().email()
-        .required("Email is required"),
-    password: Yup.string()
-        .required("Password is required"),
-    password2: Yup.string()
-        .required("Password confirmation is required"),
-});
+import {Button, Image, Container, Form, Grid, Header, Segment, Message, Label} from "semantic-ui-react";
+import logo from '../../prello_icon.png'
 
 class Register extends Component {
     constructor() {
@@ -47,7 +27,7 @@ class Register extends Component {
     componentWillReceiveProps(nextProps) {
         if (nextProps.errors) {
             this.setState({
-                errors: nextProps.errors
+                errors: nextProps.errors.message
             });
         }
     }
@@ -59,19 +39,20 @@ class Register extends Component {
             <Container>
 
                 <Grid centered textAlign='center' verticalAlign='middle'>
-                    <Grid.Column style={{maxWidth: 800}}>
-                        <Message attached>
-                            <Header as='h3'>
-                                TO DO ADD LOGO
-                                <Image src='../../../public/logo192.png'/>
-                                <Header.Content>Register to Prello</Header.Content>
-                            </Header>
-                        </Message>
+                    <Grid.Column style={{maxWidth: 700}}>
+                        <Header as='h3' textAlign='center'>
+                            <Image src={logo}/>
+                            <Header.Content>Register to Prello</Header.Content>
+                        </Header>
                         <Formik
                             initialValues={{
-                                description: ''
+                                firstName: '',
+                                lastName: '',
+                                userName: '',
+                                email: '',
+                                password: '',
+                                password2: ''
                             }}
-                            validationSchema={RegisterSchema}
                             onSubmit={values => {
 
                                 const newUser = {
@@ -87,22 +68,17 @@ class Register extends Component {
                             }}
                         >
 
-                            {({handleChange, handleSubmit, values, yupErrors}) => (
-
+                            {({handleChange, handleSubmit, values}) => (
 
                                 <Segment>
 
                                     <Form onSubmit={handleSubmit}>
                                         <Form.Group widths='equal'>
                                             <Form.Input
-                                                required
                                                 label='First Name'
                                                 placeholder='firstname'
                                                 value={values.firstName}
                                                 onChange={handleChange('firstName')}
-                                                className={classnames("", {
-                                                    invalid: errors.firstName
-                                                })}
                                                 error={errors.firstName && {content: errors.firstName}}
                                             />
                                             <Form.Input
@@ -110,45 +86,33 @@ class Register extends Component {
                                                 placeholder='lastname'
                                                 value={values.lastName}
                                                 onChange={handleChange('lastName')}
-                                                className={classnames("", {
-                                                    invalid: errors.lastName
-                                                })}
                                                 error={errors.lastName && {content: errors.lastName}}
                                             />
                                         </Form.Group>
 
                                         <Form.Group widths='equal'>
                                             <Form.Input
-                                                required
                                                 icon='user'
                                                 iconPosition='left'
                                                 label='Username'
                                                 placeholder='username'
                                                 value={values.userName}
                                                 onChange={handleChange('userName')}
-                                                className={classnames("", {
-                                                    invalid: errors.userName
-                                                })}
                                                 error={errors.userName && {content: errors.userName}}
                                             />
                                             <Form.Input
-                                                required
                                                 icon='at'
                                                 iconPosition='left'
                                                 label='Email'
                                                 placeholder='email'
                                                 value={values.email}
                                                 onChange={handleChange('email')}
-                                                className={classnames("", {
-                                                    invalid: errors.email
-                                                })}
                                                 error={errors.email && {content: errors.email}}
                                             />
                                         </Form.Group>
 
                                         <Form.Group widths='equal'>
                                             <Form.Input
-                                                required
                                                 icon='lock'
                                                 iconPosition='left'
                                                 label='Password'
@@ -156,23 +120,16 @@ class Register extends Component {
                                                 placeholder='password'
                                                 value={values.password}
                                                 onChange={handleChange('password')}
-                                                className={classnames("", {
-                                                    invalid: errors.password
-                                                })}
                                                 error={errors.password && {content: errors.password}}
                                             />
                                             <Form.Input
-                                                required
                                                 icon='lock'
                                                 iconPosition='left'
                                                 label='Confirm Password'
                                                 type='password'
-                                                placeholder='password2'
+                                                placeholder='password confirmation'
                                                 value={values.password2}
                                                 onChange={handleChange('password2')}
-                                                className={classnames("", {
-                                                    invalid: errors.password2
-                                                })}
                                                 error={errors.password2 && {content: errors.password2}}
                                             />
                                         </Form.Group>
@@ -185,7 +142,12 @@ class Register extends Component {
                         </Formik>
 
                         <Message attached='bottom' color='blue'>
-                            Already have an account? <Link to="/login"> LOG IN</Link>
+                            Already have an account?
+                            {' '}
+                            <Label as='a' color='blue' basic>
+                                <Link to="/login"> LOG IN</Link>
+                            </Label>
+
                         </Message>
 
                     </Grid.Column>
