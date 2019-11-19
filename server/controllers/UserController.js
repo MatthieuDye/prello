736bab1513d93442eafa1b482a875186c009e8e7
Gestin <gestin.remi@gmail.com>
2019-11-19 10:01:04 +0100

    polytech auth perfect

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -135,45 +135,72 @@ const UserController = () => {
 
     let userId = new ObjectID();
 
+
     // Find user by email
-    User.findOne({userName: username}).then(existingUser => {
-      if (!existingUser) {
-        new User({
-          _id: userId,
-          firstName: username.split(".")[0],
-          lastName: username.split(".")[1],
-          userName: username,
-          email: username + "@etu.umontpellier.fr",
-          password: "polytech"
-        })
-            .save()
-      }
-    }).then(a => {
+    User.findOne({userName: username})
+        .then(existingUser => {
+          if (!existingUser) {
+            new User({
+              _id: userId,
+              firstName: username.split(".")[0],
+              lastName: username.split(".")[1],
+              userName: username,
+              email: username + "@etu.umontpellier.fr",
+              password: "polytech"
+            })
+                .save()
+                .then(a => {
+                  // Create JWT Payload
+                  const payload = {
+                    id: userId,
+                    firstName: username.split(".")[0],
+                    lastName: username.split(".")[1],
+                    userName: username,
+                    email: username + "@etu.umontpellier.fr"
+                  };
 
-      // Create JWT Payload
-      const payload = {
-        id: userId,
-        firstName: username.split(".")[0],
-        lastName: username.split(".")[1],
-        userName: username,
-        email: username + "@etu.umontpellier.fr"
-      };
+                  // Sign token
+                  jwt.sign(
+                      payload,
+                      process.env.SECRET_TOKEN,
+                      {
+                        expiresIn: 3600 // 1 hour in seconds
+                      },
+                      (err, token) => {
+                        res.status(201).json({
+                          success: true,
+                          token: "Bearer " + token
+                        });
+                      }
+                  );
+                })
+          } else {
+            // Create JWT Payload
+            const payload = {
+              id: existingUser._id,
+              firstName: existingUser.firstName,
+              lastName: existingUser.lastName,
+              userName: existingUser.userName,
+              email: existingUser.email
+            };
 
-      // Sign token
-      jwt.sign(
-          payload,
-          process.env.SECRET_TOKEN,
-          {
-            expiresIn: 3600 // 1 hour in seconds
-          },
-          (err, token) => {
-            res.status(201).json({
-              success: true,
-              token: "Bearer " + token
-            });
+            // Sign token
+            jwt.sign(
+                payload,
+                process.env.SECRET_TOKEN,
+                {
+                  expiresIn: 3600 // 1 hour in seconds
+                },
+                (err, token) => {
+                  res.status(201).json({
+                    success: true,
+                    token: "Bearer " + token
+                  });
+                }
+            );
+
           }
-      );
-    })
+        })
   };
 
 
