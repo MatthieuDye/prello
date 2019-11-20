39a527e8b75103ab2cffc698ddf2a98c31a75ff9
Alia Chawaf <alia.chawaf@etu.umontpellier.fr>
2019-11-20 17:11:59 +0100

    ADD - Handle favorites boards

--- client/src/components/boards/MyBoards.js
+++ client/src/components/boards/MyBoards.js
@@ -1,145 +1,187 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { Card, Divider, Icon, Header, Container, Button } from "semantic-ui-react";
-import { Link } from "react-router-dom";
+import {connect} from "react-redux";
+import {Card, Divider, Icon, Header, Container, Button, Segment} from "semantic-ui-react";
+import {Link} from "react-router-dom";
 
 //_______ ACTIONS_______
-import { fetchBoards } from "../../actions/boardActions";
-import { favoriteBoard } from "../../actions/userActions";
+import {fetchBoards} from "../../actions/boardActions";
+import {favoriteBoard} from "../../actions/userActions";
 
 class MyBoards extends Component {
 
-  componentDidMount() {
-    this.props.fetchBoards(this.props.auth.user.id);
-  }
+    componentDidMount() {
+        this.props.fetchBoards(this.props.auth.user.id);
+    }
 
-  redirectionBoard = (boardId) => {
-    this.props.history.push(`/board/${boardId}`);
-  };
+    redirectionBoard = (boardId) => {
+        this.props.history.push(`/board/${boardId}`);
+    };
 
-  render() {
-    return (
-      <Container>
-        <Header as='h2'>
-          <Icon name='columns' />
-          <Header.Content>My Boards</Header.Content>
-          <Link to='/add/board'>
-            <Button primary size='mini' floated='right'>
-              <Icon name='add' />
-              ADD
-                    </Button>
-          </Link>
-        </Header>
+    isFavorite = (boardId) => {
+        return (this.props.favoriteBoards.filter(b => b._id === boardId).length !== 0);
+    };
 
-        <br />
+    handleFavorite = (boardId) => {
+        console.log("IN HANDLE");
+        console.log(boardId);
 
-        <Divider horizontal>
-          <Header as='h3'>
-            <Icon  name='heart' />
-            Favorite boards
-      </Header>
-        </Divider>
-        <Card.Group stackable doubling itemsPerRow={4}>
-          {this.props.favoriteBoards.map(({ _id, name, description, isFavorite }) => (
+        const isFavorite = this.isFavorite(boardId);
+        console.log(isFavorite);
+        const userID = this.props.auth.user.id;
+        console.log(userID);
+        this.props.favoriteBoard(userID, boardId, !isFavorite);
+    };
 
-              <Card>
-                <Card.Content textAlign='center' header={name} />
-                <Card.Content description={description} />
-                <Card.Content extra>
-                  {isFavorite ? <Icon name='heart' /> : <Icon name='heart outline' />}
-                  <Button onClick={() => this.redirectionBoard(_id)}> go board </Button>
-                </Card.Content>
-              </Card>
-          ))}
-        </Card.Group>
+    render() {
+        return (
+            <Container>
+                <Divider hidden/>
+                <Header as='h2'>
+                    <Segment.Inline>
+                        <Icon name='columns'/>
+                        <Header.Content>My Boards</Header.Content>
+                        <Link to='/add/board'>
+                            <Button primary size='mini' floated='right'>
+                                <Icon name='add'/>
+                                ADD
+                            </Button>
+                        </Link>
+                    </Segment.Inline>
+                </Header>
 
-        <br />
+                <Divider hidden/>
+                <Divider hidden/>
 
-        <Divider horizontal>
-          <Header as='h3'>
-            <Icon name='user' />
-            Personal boards
-      </Header>
-        </Divider>
-
-
-        <Card.Group stackable doubling itemsPerRow={4}>
-          {this.props.guestBoards.map(({ _id, name, description, isFavorite }) => (
-
-            <Card>
-              <Card.Content textAlign='center' header={name} />
-              <Card.Content description={description} />
-              <Card.Content extra>
-                {isFavorite ? <Icon name='heart' /> : <Icon name='heart outline' />}
-                <Button onClick={() => this.redirectionBoard(_id)}> go board </Button>
-              </Card.Content>
-            </Card>
-          ))}
-        </Card.Group>
-
-        <br />
-
-        {this.props.teamBoards.map(({ name, boards }) => (
-          <React.Fragment>
-            {boards.length > 0 ? (
-              <React.Fragment>
-                <Divider horizontal>
-                  <Header as='h3'>
-                    <Icon name='users' />
-                    {name}
-                  </Header>
+                {this.props.favoriteBoards.length !== 0
+                && <Divider horizontal>
+                    <Header as='h3'>
+                        <Icon name='heart'/>
+                        Favorite boards
+                    </Header>
                 </Divider>
+                }
+
                 <Card.Group stackable doubling itemsPerRow={4}>
-                  {boards.map(({ _id, name, description, isFavorite }) => (
-                    <Card>
-                      <Card.Content textAlign='center' header={name} />
-                      <Card.Content description={description} />
-                      <Card.Content extra>
-                        {isFavorite ? <Icon name='heart' /> : <Icon name='heart outline' />}
-                        <Button onClick={() => this.redirectionBoard(_id)}> go board </Button>
-                      </Card.Content>
-                    </Card>
-                  ))}
+                    {this.props.favoriteBoards.map(({_id, name, description}) => (
+
+                        <Card color='red'>
+                            <Card.Content as={'a'} onClick={() => this.redirectionBoard(_id)} textAlign='center'
+                                          header={name}/>
+                            <Card.Content as={'a'} onClick={() => this.redirectionBoard(_id)}
+                                          description={description}/>
+                            <Card.Content extra>
+                                <Icon
+                                    size='large'
+                                    color='red'
+                                    name='heart outlet'
+                                    link
+                                    onClick={() => this.handleFavorite(_id)}
+                                />
+                            </Card.Content>
+                        </Card>
+                    ))}
                 </Card.Group>
-              </React.Fragment>
-            ) : (
-                <p></p>
-              )}
 
+                <Divider hidden/>
+                <Divider hidden/>
 
-          </React.Fragment>
+                {this.props.guestBoards.length !== 0 &&
+                <Divider horizontal>
+                    <Header as='h3'>
+                        <Icon name='user'/>
+                        Personal boards
+                    </Header>
+                </Divider>
+                }
 
-        ))}
+                <Card.Group stackable doubling itemsPerRow={4}>
+                    {this.props.guestBoards.map(({_id, name, description}) => (
 
-      </Container>
-    );
-  }
+                        <Card color='blue'>
+                            <Card.Content as={'a'} onClick={() => this.redirectionBoard(_id)} textAlign='center'
+                                          header={name}/>
+                            <Card.Content as={'a'} onClick={() => this.redirectionBoard(_id)} textAlign='center'
+                                          description={description}/>
+                            <Card.Content extra>
+                                <Icon
+                                    size='large'
+                                    color='red'
+                                    name={this.isFavorite(_id) ? 'heart' : 'heart outline'}
+                                    link
+                                    onClick={() => this.handleFavorite(_id)}
+                                />
+                            </Card.Content>
+                        </Card>
+                    ))}
+                </Card.Group>
+
+                <Divider hidden/>
+                <Divider hidden/>
+
+                {this.props.teamBoards.map(({name, boards}) => (
+                    <React.Fragment>
+                        {boards.length > 0 && (
+                            <React.Fragment>
+                                <Divider horizontal>
+                                    <Header as='h3'>
+                                        <Icon name='users'/>
+                                        {name}
+                                    </Header>
+                                </Divider>
+                                <Card.Group stackable doubling itemsPerRow={4}>
+                                    {boards.map(({_id, name, description}) => (
+                                        <Card color='blue'>
+                                            <Card.Content as={'a'} onClick={() => this.redirectionBoard(_id)}
+                                                          textAlign='center' header={name}/>
+                                            <Card.Content as={'a'} onClick={() => this.redirectionBoard(_id)}
+                                                          textAlign='center' description={description}/>
+                                            <Card.Content extra>
+                                                <Icon
+                                                    size='large'
+                                                    color='red'
+                                                    name={this.isFavorite(_id) ? 'heart' : 'heart outline'}
+                                                    link
+                                                    onClick={() => this.handleFavorite(_id)}
+                                                />
+                                            </Card.Content>
+                                        </Card>
+                                    ))}
+                                </Card.Group>
+                            </React.Fragment>
+                        )}
+                    </React.Fragment>
+
+                ))}
+
+            </Container>
+        );
+    }
 }
 
 MyBoards.propTypes = {
-  guestBoards: PropTypes.arrayOf(PropTypes.object).isRequired,
-  teamBoards: PropTypes.arrayOf(PropTypes.object).isRequired,
-  fetchBoards: PropTypes.func.isRequired,
-  favoriteBoard: PropTypes.func.isRequired,
-  favoriteBoards: PropTypes.arrayOf(PropTypes.object).isRequired,
-  auth: PropTypes.object.isRequired,
+    guestBoards: PropTypes.arrayOf(PropTypes.object).isRequired,
+    teamBoards: PropTypes.arrayOf(PropTypes.object).isRequired,
+    fetchBoards: PropTypes.func.isRequired,
+    favoriteBoard: PropTypes.func.isRequired,
+    favoriteBoards: PropTypes.arrayOf(PropTypes.object).isRequired,
+    auth: PropTypes.object.isRequired,
 };
 
 MyBoards.defaultProps = {
-  guestBoards: [],
-  teamBoards: [],
-  favoriteBoards: []
+    guestBoards: [],
+    teamBoards: [],
+    favoriteBoards: []
 };
 
 const mapStateToProps = state => ({
-  guestBoards: state.boards.guestBoards,
-  teamBoards: state.boards.teamsBoards,
-  favoriteBoards: state.boards.favoriteBoards,
-  auth: state.auth
+    guestBoards: state.boards.guestBoards,
+    teamBoards: state.boards.teamsBoards,
+    favoriteBoards: state.boards.favoriteBoards,
+    auth: state.auth
 });
 
 export default connect(
-  mapStateToProps,
-  { fetchBoards, favoriteBoard }
+    mapStateToProps,
+    {fetchBoards, favoriteBoard}
 )(MyBoards);
