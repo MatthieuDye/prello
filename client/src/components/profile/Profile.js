7a704792464e2880beec852fac08abb398cd9ac2
Gestin <gestin.remi@gmail.com>
2019-11-22 00:48:16 +0100

    [FRONT] Formik profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,7 +3,10 @@ import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import classnames from "classnames";
+import {Button, Container, Divider, Form, Grid, Header, Image, Label, Message, Segment} from "semantic-ui-react";
 import { updateUser, fetchUser } from "../../actions/userActions";
+import {Formik} from "formik";
+
 
 class Profile extends Component {
   constructor() {
@@ -57,117 +60,91 @@ class Profile extends Component {
     this.setState({ [e.target.id]: e.target.value });
   };
 
-  onSubmit = e => {
-    e.preventDefault();
+    render() {
+        const {errors} = this.state;
 
-    const updatedUser = {
-      firstName: this.state.firstName,
-      lastName: this.state.lastName,
-      userName: this.state.userName,
-      email: this.state.email
-    };
+        return (
+            <Container>
+                <Grid centered textAlign='center' verticalAlign='middle'>
+                    <Grid.Column style={{maxWidth: 500}}>
 
-    this.props.updateUser(this.props.user.userName, updatedUser, this.props.history);
-  };
+                        <div className="col s12" style={{paddingLeft: "11.250px"}}>
+                            <h4>
+                                <b>Manage your profile</b>
+                            </h4>
+                        </div>
+                        <Formik initialValues={{
+                            firstName: this.state.firstName,
+                            lastName: this.state.lastName,
+                            userName: this.state.userName,
+                            email: this.state.email
+                        }}
+                                enableReinitialize
+                                onSubmit={values => {
 
-  render() {
-    const { errors } = this.state;
+                            const updatedUser = {
+                                firstName: values.firstName,
+                                lastName: values.lastName,
+                                userName: values.userName,
+                                email: values.email
+                            };
 
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col s8 offset-s2">
-            <Link to="/" className="btn-flat waves-effect">
-              <i className="material-icons left">keyboard_backspace</i> Back to
-              home
-            </Link>
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Manage your profile</b>
-              </h4>
-            </div>
-            <form noValidate onSubmit={this.onSubmit}>
+                            this.props.updateUser(this.props.user.userName, updatedUser, this.props.history);
+                        }}>
 
-            <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.firstName}
-                  error={errors.firstName}
-                  id="firstName"
-                  type="text"
-                  className={classnames("", {
-                    invalid: errors.firstName
-                  })}
-                />
-                <label htmlFor="firstName">First Name</label>
-                <span className="red-text">{errors.firstName}</span>
-              </div>
-              
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.lastName}
-                  error={errors.lastName}
-                  id="lastName"
-                  type="text"
-                  className={classnames("", {
-                    invalid: errors.lastName
-                  })}
-                />
-                <label htmlFor="lastName">Last Name</label>
-                <span className="red-text">{errors.lastName}</span>
-              </div>
-              
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.userName}
-                  error={errors.userName}
-                  id="userName"
-                  type="text"
-                  className={classnames("", {
-                    invalid: errors.userName
-                  })}
-                />
-                <label htmlFor="userName">User Name</label>
-                <span className="red-text">{errors.userName}</span>
-              </div>
+                            {({handleChange, handleSubmit, values}) => (
 
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.email}
-                  error={errors.email}
-                  id="email"
-                  type="email"
-                  className={classnames("", {
-                    invalid: errors.email
-                  })}
-                />
-                <label htmlFor="email">Email</label>
-                <span className="red-text">{errors.email}</span>
-              </div>
-              
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem"
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Save
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
+                                <Segment>
+
+                                    <Form onSubmit={handleSubmit}>
+                                        <Form.Input
+                                            label='First Name'
+                                            placeholder='firstname'
+                                            value={values.firstName}
+                                            onChange={handleChange('firstName')}
+                                            error={errors.firstName && {content: errors.firstName}}
+                                        />
+
+
+                                        <Form.Input
+                                            label='Last Name'
+                                            placeholder='lastname'
+                                            value={values.lastName}
+                                            onChange={handleChange('lastName')}
+                                            error={errors.lastName && {content: errors.lastName}}
+                                        />
+
+                                        <Form.Input
+                                            icon='user'
+                                            iconPosition='left'
+                                            label='Username'
+                                            placeholder='username'
+                                            value={values.userName}
+
+                                            error={errors.userName && {content: errors.userName}}
+                                        />
+
+
+                                        <Form.Input
+                                            icon='at'
+                                            iconPosition='left'
+                                            label='Email'
+                                            placeholder='email'
+                                            value={values.email}
+
+                                            error={errors.email && {content: errors.email}}
+                                        />
+
+                                        <Button type='submit' content='Save' primary/>
+                                    </Form>
+                                </Segment>
+                            )}
+                        </Formik>
+                    </Grid.Column>
+                </Grid>
+            </Container>
+        );
+    }
 }
 
 Profile.propTypes = {
